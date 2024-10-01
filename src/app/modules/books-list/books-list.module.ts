adb89ecce21cd21c83a542023f8b09846efc3c70
hideri13 <evilincshop@yandex.ru>
2024-10-01 15:46:18 +0300

    Added Grotem Base Table

--- src/app/modules/books-list/books-list.module.ts
+++ src/app/modules/books-list/books-list.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { BooksListRoutingModule } from './books-list-routing.module';
 import { ListComponent } from './components/list/list.component';
+import { GuiTableModule } from '@grotem-ui/grotem-ui-lib';
 
 @NgModule({
   declarations: [ListComponent],
-  imports: [CommonModule, BooksListRoutingModule],
+  imports: [CommonModule, BooksListRoutingModule, GuiTableModule],
 })
 export class BooksListModule {}
