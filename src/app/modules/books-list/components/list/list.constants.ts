2e623faa24ac7319c6abf063a6a9e42fdf009f6f
hideri13 <evilincshop@yandex.ru>
2024-10-03 12:07:44 +0300

    Paginator Finished

--- src/app/modules/books-list/components/list/list.constants.ts
+++ src/app/modules/books-list/components/list/list.constants.ts
@@ -1,12 +1,7 @@
 export const stockPaginatorOptions = {
-  pageSize: 25,
+  pageSize: 10,
   pageIndex: 0,
-  pageSizeOptions: [5, 10, 15, 20, 25, 30, 50, 100, 200],
+  pageSizeOptions: [10, 15, 20, 30, 50, 100, 200],
   totalLength: 0,
-  hidePageSize: false,
-};
-
-export const BookListInfoMsg = {
-  loading: 'Loading books!',
-  error: 'Error occurred while fetching books. Please refresh the page.',
+  hidePageSize: true,
 };
