c9de8b7fdd08d6854f291ba2502e2c08da859533
hideri13 <evilincshop@yandex.ru>
2024-10-22 11:49:40 +0300

    Typing fix

--- src/app/pages/books-list/ui/list/list.constants.ts
+++ src/app/pages/books-list/ui/list/list.constants.ts
@@ -1,6 +1,6 @@
 import { PaginatorParams } from './paginator-params.interface';
 
-export const stockPaginatorOptions = <PaginatorParams>{
+export const stockPaginatorOptions: PaginatorParams = <PaginatorParams>{
   pageSize: 25,
   pageIndex: 0,
   pageSizeOptions: [5, 10, 15, 20, 25, 30, 50, 100, 200],
